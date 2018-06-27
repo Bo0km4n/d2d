0f8ffa0d9577c573543295e7f0141c2e2c75dd52
Bo0km4n <pokopenkatuya@gmail.com>
2018-06-27 14:54:44 +0900

    add aggregated model

--- infra/collector/model/log.go
+++ infra/collector/model/log.go
@@ -17,3 +17,17 @@ type Log struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+type AggregatedLog struct {
+	ID           int `gorm:"primary_key"`
+	ElapsedTime  uint64
+	LambdaAccelX int
+	LambdaAccelY int
+	LambdaAccelZ int
+	LambdaGyroX  int
+	LambdaGyroY  int
+	LambdaGyroZ  int
+	LambdaMagX   int
+	LambdaMagY   int
+	LambdaMagZ   int
+}
