abba756cddff3611315857cb299117add0536c10
Bo0km4n <pokopenkatuya@gmail.com>
2018-07-11 14:37:29 +0900

    add save model

--- ml/batch/svm.py
+++ ml/batch/svm.py
@@ -2,6 +2,7 @@ import os, sys
 sys.path.append(os.getcwd())
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.externals import joblib
 
 class SVM:
     def __init__(self, x_train, y_train):
@@ -17,3 +18,12 @@ class SVM:
         accuracy_train = accuracy_score(self.y_train, pred_train)
         print('トレーニングデータに対する正解率： %.2f' % accuracy_train)
     
+    def pred(self, x):
+        pred = self.model.predict(x)
+        return pred
+    
+    def save_model(self):
+        joblib.dump(self.model, 'svm.pkl')
+
+    def load_model(self):
+        self.model = joblib.load('svm.pkl')
